c7a4f9fa196a9edbff9a2a9a56806a160b5811d1
Aporteli <giorgi101mikolaichukis@gmail.com>
2026-08-24 08:46:10 +0400

    Clean next.config.ts options

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   typescript: {
-    // აიგნორებს ტიპიზაციის შეცდომებს build-ის დროს, რათა დეპლოი შეუფერხებლად დასრულდეს
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // აიგნორებს ლინტერის შეცდომებს build-ის დროს
-    ignoreDuringBuilds: true,
   },
   experimental: {
     serverActions: {
