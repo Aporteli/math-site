44c65c2e835ffa8e308828e582f00d83936d1b03
Aporteli <giorgi101mikolaichukis@gmail.com>
2026-08-26 21:23:28 +0400

    join

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaTiDBCloud } from "@tidbcloud/prisma-adapter";
 
 const PRISMA_GENERATION = "taxonomy-nodes-v1";
 
@@ -14,8 +14,7 @@ function createPrismaClient(): PrismaClient {
     throw new Error("DATABASE_URL is not set");
   }
 
-  const adapter = new PrismaMariaDb(url);
-
+  const adapter = new PrismaTiDBCloud(url);
   return new PrismaClient({ adapter });
 }
 
