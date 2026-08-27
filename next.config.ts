a5fc6a0c3e82b1a4222c1362aed0d634ddb1507f
Aporteli <giorgi101mikolaichukis@gmail.com>
2026-08-27 09:14:10 +0400

    join

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['192.168.1.242', 'localhost:3000'],
   reactCompiler: true,
   typescript: {
     ignoreBuildErrors: true,
